6a5b299f1589bf1f17c1d6b5dececbf6697e0de4
nick-hiebl <n.hiebl@student.unsw.edu.au>
2019-07-20 11:22:56 +1000

    Simple Canvas consumer for testing/demonstration purposes

---
+++ main.ts
@@ -0,0 +1,58 @@
+import { addSetup, addUpdate, Canvas } from './visual';
+
+var canvas: Canvas;
+
+var width: number;
+var height: number;
+
+interface KeysHeld {
+    [key: string]: boolean;
+};
+
+var keysHeld: KeysHeld = {};
+
+// document.addEventListener('mousemove', (event) => {
+//     x = event.x - offsetX;
+//     y = event.y - offsetY;
+// });
+
+document.addEventListener('keydown', (event) => {
+    keysHeld[event.key] = true;
+});
+
+document.addEventListener('keyup', (event) => {
+    keysHeld[event.key] = false;
+});
+
+function setup() {
+    canvas = new Canvas("canvas");
+
+    resize();
+
+    addUpdate(mainLoop);
+}
+
+function resize() {
+    if (width != window.innerWidth || height != window.innerHeight) {
+        console.warn('Expensive resize operation');
+        width = window.innerWidth;
+        height = window.innerHeight;
+        canvas.resize(width, height);
+    }
+}
+
+function draw() {
+    canvas.clear();
+
+    canvas.color('black');
+    canvas.background();
+
+    canvas.color('red');
+    canvas.fillArc(200, 200, 100);
+}
+
+function mainLoop() {
+    draw();
+}
+
+addSetup(setup);
